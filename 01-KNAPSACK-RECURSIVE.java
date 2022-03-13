ef8ad8bbe79f2ff4cceb93ad9c54d7b17d8fadf5
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-03-13 18:37:20 +0530

    first commit

---
+++ 01-KNAPSACK-RECURSIVE.java
@@ -0,0 +1,24 @@
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here
+         if(W == 0 || n == 0) return 0;
+         
+         if(wt[n-1] <= W) {
+             return Math.max(val[n-1] + knapSack(W-wt[n-1] , wt , val , n-1) ,
+             
+                knapSack(W , wt , val , n-1));
+             
+         }
+         
+         else if(wt[n-1] > W){
+             
+             return knapSack(W , wt , val , n-1); 
+         }
+         
+         return 0;
+         
+    } 
+}
