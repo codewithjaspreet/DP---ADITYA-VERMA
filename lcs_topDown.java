984d4f798e9dbb4c430f6d9bfb9ab7890d59075b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-24 19:00:54 +0530

    longest common subsequence top down approach solution

---
+++ lcs_topDown.java
@@ -0,0 +1,41 @@
+public class lcs_topDown {
+        public int longestCommonSubsequence(String text1, String text2) {
+
+            int n = text1.length();
+            int m = text2.length();
+
+            int[][] dp = new int[1001][1001];
+
+
+            for(int i = 0 ; i < n+1 ; i++){
+
+                for(int j = 0 ; j < m + 1 ; j++){
+
+                    if(i == 0 || j == 0){
+                        dp[i][j] = 0;
+                    }
+                }
+            }
+
+            for(int i = 1 ; i < n+1 ; i++){
+                for(int  j = 1 ; j < m +1  ; j++){
+
+                    if(text1.charAt(i-1) == text2.charAt(j-1)){
+
+                        dp[i][j] =  1 + dp[i-1][j-1];
+                    }
+
+                    else{
+
+                        dp[i][j] = Math.max(dp[i][j-1] , dp[i-1][j]);
+                    }
+                }
+            }
+            return dp[n][m];
+
+
+        }
+
+
+
+    }
