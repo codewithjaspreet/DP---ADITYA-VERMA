c05fe92d7e73379b43078bb450d2599a6143efb3
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-25 07:11:46 +0530

    Matrix chain multiplication recursive solution

---
+++ mcm_recursive.java
@@ -0,0 +1,28 @@
+// step 1 :  find i and j
+// step 2 : find base condition
+// step 3 : find k loop scheme
+// step 4 : calculate temp ans then apply the function to get final ans
+// great article to refer :
+
+//  * https://leetcode.com/discuss/general-discussion/1278305/all-about-matrix-chain-multiplication-easy *
+
+public class mcm_recursive {
+    int solve(int i, int j,int arr[])
+    {
+        if(i>=j)return 0;
+
+        int ans=Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++)
+        {
+            int tempAns = solve(i,k,arr) + solve(k+1,j,arr)
+                    + arr[i-1]*arr[k]*arr[j];
+
+            ans=Math.min(ans,tempAns);
+        }
+        return ans;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        return solve(1,N-1,arr);
+    }
+}
